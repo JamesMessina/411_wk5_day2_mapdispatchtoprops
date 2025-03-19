2250565205f43416a189796e01dc1f1145367642
James Messina <jmessina17@gmail.com>
2025-03-19 16:11:34 -0500

    updating to hash router and now using git pages

--- src/App.js
+++ src/App.js
@@ -3,16 +3,17 @@ import Navigation from './components/Navigation'
 import './App.css'
 import Router from './Router'
 import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux'
 import store from './redux/store'
 
 function App() {
   return (
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <Navigation />
         <Router />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   );
 }
