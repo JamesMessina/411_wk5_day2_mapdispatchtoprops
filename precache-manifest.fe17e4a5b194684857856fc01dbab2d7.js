fd24d1d485b31c8f64eee73969c8ac93f51ec387
James Messina <jmessina17@gmail.com>
2025-03-19 16:14:22 -0500

    Updates

---
+++ precache-manifest.fe17e4a5b194684857856fc01dbab2d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d82996854ae076f374d8335860cb9135",
+    "url": "/411_wk5_day2_mapdispatchtoprops/index.html"
+  },
+  {
+    "revision": "bdcce6196ecf3733f34a",
+    "url": "/411_wk5_day2_mapdispatchtoprops/static/css/main.be9515fe.chunk.css"
+  },
+  {
+    "revision": "2472e18ca9a014df0f4e",
+    "url": "/411_wk5_day2_mapdispatchtoprops/static/js/2.58827aad.chunk.js"
+  },
+  {
+    "revision": "bdcce6196ecf3733f34a",
+    "url": "/411_wk5_day2_mapdispatchtoprops/static/js/main.a4dff22b.chunk.js"
+  },
+  {
+    "revision": "e47b0be5e9874947a62d",
+    "url": "/411_wk5_day2_mapdispatchtoprops/static/js/runtime~main.df0822c9.js"
+  }
+]);
